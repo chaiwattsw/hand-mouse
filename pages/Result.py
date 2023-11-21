0cc9cc6cde684b962298d01c1e2fa096d1d91131
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2023-11-21 08:40:14 +0700

    fix

--- pages/Result.py
+++ pages/Result.py
@@ -65,6 +65,7 @@ def result_page():
         rtc_configuration={
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
         },
+        async_processing=True,
     )
 
 result_page()
