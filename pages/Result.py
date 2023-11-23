886c583aa9489014f5fc88d6f663113c0687888a
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2023-11-24 02:20:05 +0700

    fix

--- pages/Result.py
+++ pages/Result.py
@@ -1,4 +1,5 @@
 import mediapipe as mp
+import numpy as np
 import cv2
 import subprocess
 import streamlit as st
@@ -18,38 +19,38 @@ def set_volume(volume):
 
 # Hand gesture recognition and volume control
 def video_frame_callback(frame):
-    image = frame.to_ndarray(format="bgr24")
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+     image = frame.to_ndarray(format="bgr24")
+     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    current_volume = 50  # Initialize with a default volume value
+     volume = 50  # Initial volume level
+     pinch_threshold = 0.05  # Threshold to differentiate touch from apart
 
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+     if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            # Get finger landmarks
-            thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-            index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-            thumb_x, thumb_y = thumb_tip.x, thumb_tip.y
-            index_x, index_y = index_tip.x, index_tip.y
+                # Get finger landmarks
+                thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                thumb_x, thumb_y = thumb_tip.x, thumb_tip.y
+                index_x, index_y = index_tip.x, index_tip.y
 
-            # Calculate distance between thumb and index finger landmarks
-            distance = ((thumb_x - index_x) ** 2 + (thumb_y - index_y) ** 2) ** 0.5
-            pinch_threshold = 0.05  # Threshold to differentiate touch from apart
+                # Calculate distance between thumb and index finger landmarks
+                distance = ((thumb_x - index_x) ** 2 + (thumb_y - index_y) ** 2) ** 0.5
 
-            # Adjust volume based on hand gesture
-            if distance < pinch_threshold:
-                current_volume -= 1 if current_volume > 0 else 0  # Decrease volume
-            else:
-                current_volume += 1 if current_volume < 100 else 0  # Increase volume
+                # Adjust volume based on hand gesture distance
+                if distance > pinch_threshold:  # Check if the distance is above the threshold
+                    volume -= 1 if volume > 0 else 0  # Decrease volume when distance is far
+                else:
+                    volume += 1 if volume < 100 else 0  # Increase volume when distance is near
 
-            # Set volume using subprocess
-            set_volume(current_volume)
+                # Set volume using subprocess
+                set_volume(volume)
 
-            # Display current volume level in the frame
-            cv2.putText(image, f"Volume: {current_volume}", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                # Display current volume level in the frame
+                cv2.putText(image, f"Volume: {volume}", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
-    return av.VideoFrame.from_ndarray(image, format="bgr24")
+     return av.VideoFrame.from_ndarray(image, format="bgr24")
 
 def result_page():
     st.title("Hand Gesture Volume Control - Result")
@@ -58,12 +59,13 @@ def result_page():
 
     webrtc_streamer(
         key="hand_gesture_volume_control", 
+        mode=WebRtcMode.SENDRECV,
         video_frame_callback=video_frame_callback,
         media_stream_constraints={"video": True, "audio": False},
-        rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-        },
         async_processing=True,
+         rtc_configuration={
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+        }
     )
 
 result_page()
